e269b5e8dd5da39c630c06ad905ac1a242f548b6
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-05-22 23:22:56 +1200

    Fix deployment script issue

--- deploy.cmd
+++ deploy.cmd
@@ -1,4 +1,4 @@
-D:\home\python362x86\python.exe python -m pip install --upgrade pip
+D:\home\python362x86\python.exe -m pip install --upgrade pip
 D:\home\python362x86\python.exe -m pip install -I setuptools
 D:\home\python362x86\python.exe -m pip install --upgrade -r requirements.txt
 D:\home\python362x86\python.exe demo/manage.py migrate
