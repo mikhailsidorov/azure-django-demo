dd4d07eeebab047dc0d7d30f210af1a422ba6939
Mikhail Sidorov <7780462+mikhailsidorov@users.noreply.github.com>
2018-05-22 23:49:28 +1200

    Fix settings and start script

--- run_waitress_server.py
+++ run_waitress_server.py
@@ -1,7 +1,8 @@
 import os
 import sys
-from waitress import serve  
+from waitress import serve
 sys.path.insert(0, './demo')
 from demo.wsgi import application
 
-serve(application, host="0.0.0.0", port=os.environ["PORT"])
+port = os.environ.get('PORT') or 8000
+serve(application, host="0.0.0.0", port=port)
